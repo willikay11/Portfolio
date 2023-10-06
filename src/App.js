d5364fd30c367a2bb5a5c8800248a23e2da163eb
William Kamau <william.kamuyu@gapstack.com>
2023-10-07 00:39:44 +0300

    Update tailwind config

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import solgates from './assets/images/solgates.svg';
 import brck from './assets/images/BRCK.png';
 import ajua from './assets/images/AJUA.png';
 import cytonn from './assets/images/cytonn.png';
-import {RiCheckDoubleLine, RiFile2Line, RiGithubLine, RiMailLine, RiMessage2Line, RiPhoneLine} from "react-icons/ri";
+import {RiCheckDoubleLine, RiFile2Line, RiGithubLine, RiMailLine, RiPhoneLine} from "react-icons/ri";
+import {useState} from "react";
 
 function App() {
+  const [projectType, setProjectType] = useState('work');
   return (
       <div className="w-full bg-white h-screen p-4 mb-2">
           <div className="grid grid-cols-12 h-full">
-            <div className="col-span-12 md:col-start-2 md:col-span-11 xl:col-start-3 xl:col-span-8 2xl:col-start-5 2xl:col-span-4">
+            <div className="col-span-12 md:col-start-2 md:col-span-11 xl:col-start-3 xl:col-span-8 2xl:col-start-4 2xl:col-span-5 3xl:col-start-5 3xl:col-span-4">
                 <p className="text-gray-600 text-x13 font-normal uppercase 2xl::text-x15 3xl:text-base">Menu</p>
                 <div className="inline-flex mt-3">
                     <span className="text-gray-900 text-x13 font-[900] xl:text-base">Home&nbsp;/</span>
@@ -26,72 +28,81 @@ function App() {
 
                 <div className="grid grid-cols-12">
                     <div className="col-span-8 mt-4 xl:mt-8">
-                        <p className="text-gray-900 font-[900] text-4xl xl:text-5xl">Whatever the design is</p>
-                        <p className="text-gray-900 font-[900] text-4xl xl:text-5xl mt-2 xl:mt-4">I always <span className="text-rose-500">implement</span> it</p>
+                        <p className="text-gray-900 font-[900] text-4xl xl:text-4xl">Code Artist at Work</p>
+                        <p className="text-gray-900 font-[900] text-4xl xl:text-4xl mt-2 xl:mt-4">Discover the <span className="text-rose-500">Artistry</span> of a Senior Engineer</p>
 
                         <section>
                             <p className="text-gray-900 uppercase text-x8 font-[900] mt-10 xl:text-x15">ABOUT ME</p>
 
-                            <p className="text-gray-900 text-x13 mt-2.5 w-3/4">
-                                Hello, my name is William. Generative AI has hit the ground running so fast that it can feel hard to keep up. Here’s a quick take pulled from our top articles and reports on the subject. Generative AI has hit the ground running so fast that it can feel hard to keep.
+                            <p className="text-gray-900 text-x13 mt-2.5 w-3/4 lg:text-sm">
+                                Hello, my name is William, a passionate code craftsman who believes that love is at the heart of his work. With years of experience in the tech industry, my dedication to my craft shines through in every project I touch. My commitment to excellence and attention to detail are evident in the elegant products I creat, where each line of code is meticulously crafted with care.
                             </p>
                         </section>
 
                         <section>
                             <p className="text-gray-900 uppercase text-x8 font-[900] mt-10 xl:text-x15">TECH STACK</p>
 
-                            <p className="text-gray-900 text-x13 mt-2.5 w-3/4">
-                                This is some placeholder text. Iam passionate about helping creatives and entrepreneurial teams meet their goals and achieve what they set out to do through strategic
+                            <p className="text-gray-900 text-x13 mt-2.5 w-3/4 lg:text-sm">
+                                My frontend tech stack is a robust arsenal of cutting-edge technologies, ensuring I can create immersive and responsive user interfaces
                             </p>
                             <div className="inline-flex mt-3">
-                                <span className="mr-2 text-gray-900 text-x13 inline-flex items-center"><RiCheckDoubleLine className="mr-1" />React</span>
-                                <span className="mr-2 text-gray-900 text-x13 inline-flex items-center"><RiCheckDoubleLine className="mr-1" />Angular</span>
-                                <span className="mr-2 text-gray-900 text-x13 inline-flex items-center"><RiCheckDoubleLine className="mr-1" />Flutter</span>
-                                <span className="mr-2 text-gray-900 text-x13 inline-flex items-center"><RiCheckDoubleLine className="mr-1" />Javascript</span>
-                                <span className="mr-2 text-gray-900 text-x13 inline-flex items-center"><RiCheckDoubleLine className="mr-1" />Node JS</span>
-                                <span className="mr-2 text-gray-900 text-x13 inline-flex items-center"><RiCheckDoubleLine className="mr-1" />Remix</span>
+                                <span className="mr-2 text-gray-900 text-x13 lg:text-sm inline-flex items-center"><RiCheckDoubleLine className="mr-1" />React</span>
+                                <span className="mr-2 text-gray-900 text-x13 lg:text-sm inline-flex items-center"><RiCheckDoubleLine className="mr-1" />Angular</span>
+                                <span className="mr-2 text-gray-900 text-x13 lg:text-sm inline-flex items-center"><RiCheckDoubleLine className="mr-1" />Flutter</span>
+                                <span className="mr-2 text-gray-900 text-x13 lg:text-sm inline-flex items-center"><RiCheckDoubleLine className="mr-1" />Javascript</span>
+                                <span className="mr-2 text-gray-900 text-x13 lg:text-sm inline-flex items-center"><RiCheckDoubleLine className="mr-1" />Node JS</span>
+                                <span className="mr-2 text-gray-900 text-x13 lg:text-sm inline-flex items-center"><RiCheckDoubleLine className="mr-1" />Remix</span>
                             </div>
                         </section>
 
                         <section>
-                            <p className="text-gray-900 uppercase text-x8 font-[900] mt-10 xl:text-x15">WORK EXPERIENCE/PERSONAL PROJECTS</p>
-                            <div className="grid grid-cols-2 gap-6 mt-2.5">
-                                <div>
-                                    <img src={gapstack} className="mb-2" alt="Gapstack Logo"/>
-                                    <p className="text-gray-900 text-x13">
-                                        <strong>Gapstack:</strong> This is some placeholder text. I am passionate about helping creatives and entrepreneurial.
-                                    </p>
-                                </div>
-
-                                <div>
-                                    <img src={solgates} className="mb-2" alt="Solgates Logo"/>
-                                    <p className="text-gray-900 text-x13">
-                                        <strong>Solgates:</strong> This is some placeholder text. I am passionate about helping creatives and entrepreneurial.
-                                    </p>
-                                </div>
-
-                                <div>
-                                    <img src={brck} className="mb-2" alt="BRCK Logo"/>
-
-                                    <p className="text-gray-900 text-x13">
-                                        <strong>BRCK:</strong> This is some placeholder text. I am passionate about helping creatives and entrepreneurial.
-                                    </p>
-                                </div>
-
-                                <div>
-                                    <img src={ajua} className="mb-2" alt="AJUA Logo"/>
-                                    <p className="text-gray-900 text-x13">
-                                        <strong>AJUA:</strong> This is some placeholder text. I am passionate about helping creatives and entrepreneurial.
-                                    </p>
-                                </div>
-
-                                <div>
-                                    <img src={cytonn} className="mb-2" alt="Cytonn Logo"/>
-                                    <p className="text-gray-900 text-x13">
-                                        <strong>Cytonn Investments:</strong> This is some placeholder text. I am passionate about helping creatives and entrepreneurial.
-                                    </p>
-                                </div>
+                            <div className="text-gray-900 uppercase text-x8 font-[900] mt-10 xl:text-x15">
+                                <span className={`${projectType === 'work' ? 'text-rose-500' : ''} cursor-pointer`} onClick={() => setProjectType('work')}>WORK EXPERIENCE</span>&nbsp;/&nbsp;
+                                <span className={`${projectType === 'personal' ? 'text-rose-500' : ''} cursor-pointer`} onClick={() => setProjectType('personal')}>PERSONAL PROJECTS</span>
                             </div>
+                            {
+                                projectType === 'work' ? (
+                                    <div className="grid grid-cols-2 gap-6 mt-2.5">
+                                        <div>
+                                            <img src={gapstack} className="mb-2" alt="Gapstack Logo"/>
+                                            <p className="text-gray-900 text-x13 lg:text-sm">
+                                                <strong>Gapstack:</strong> This is some placeholder text. I am passionate about helping creatives and entrepreneurial.
+                                            </p>
+                                        </div>
+
+                                        <div>
+                                            <img src={brck} className="mb-2" alt="BRCK Logo"/>
+
+                                            <p className="text-gray-900 text-x13 lg:text-sm">
+                                                <strong>BRCK:</strong> This is some placeholder text. I am passionate about helping creatives and entrepreneurial.
+                                            </p>
+                                        </div>
+
+                                        <div>
+                                            <img src={ajua} className="mb-2" alt="AJUA Logo"/>
+                                            <p className="text-gray-900 text-x13 lg:text-sm">
+                                                <strong>AJUA:</strong> This is some placeholder text. I am passionate about helping creatives and entrepreneurial.
+                                            </p>
+                                        </div>
+
+                                        <div>
+                                            <img src={cytonn} className="mb-2" alt="Cytonn Logo"/>
+                                            <p className="text-gray-900 text-x13 lg:text-sm">
+                                                <strong>Cytonn Investments:</strong> This is some placeholder text. I am passionate about helping creatives and entrepreneurial.
+                                            </p>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <div className="grid grid-cols-2 gap-6 mt-2.5">
+                                        <div>
+                                            <img src={solgates} className="mb-2" alt="Solgates Logo"/>
+                                            <p className="text-gray-900 text-x13 lg:text-sm">
+                                                <strong>Solgates:</strong> This is some placeholder text. I am passionate about helping creatives and entrepreneurial.
+                                            </p>
+                                        </div>
+                                    </div>
+                                )
+                            }
                         </section>
                     </div>
                     <div className="col-span-4 mt-4 xl:mt-12 ml-0 md:ml-4 xl:ml-8 2xl:ml-16">
@@ -103,31 +114,31 @@ function App() {
 
                         <p className="uppercase text-gray-600 text-x8 mt-4">ELSEWHERE</p>
 
-                        <div className="mt-1.5">
-                            <div className="text-x13 text-rose-500 inline-flex items-center justify-center">
+                        <div className="mt-1.5 cursor-pointer" onClick={() => window.open('mailto:kamuyu.will@gmail.com', '_blank')}>
+                            <div className="text-x13 lg:text-sm text-rose-500 inline-flex items-center justify-center">
                                 <RiMailLine className="mr-1.5" /> Email
                             </div>
                         </div>
 
-                        <div className="mt-1">
-                            <div className="text-x13 text-rose-500 inline-flex items-center justify-center">
+                        <div className="mt-1 cursor-pointer" onClick={() => window.open('https://github.com/willikay11', '_blank')}>
+                            <div className="text-x13 lg:text-sm text-rose-500 inline-flex items-center justify-center">
                                 <RiGithubLine className="mr-1.5" /> Github
                             </div>
                         </div>
 
                         <div className="mt-1">
-                            <div className="text-x13 text-rose-500 inline-flex items-center justify-center">
+                            <div className="text-x13 lg:text-sm text-rose-500 inline-flex items-center justify-center">
                                 <RiFile2Line className="mr-1.5" /> CV
                             </div>
                         </div>
 
-                        <div className="mt-1">
-                            <div className="text-x13 text-rose-500 inline-flex items-center justify-center">
+                        <div className="mt-1 cursor-pointer" onClick={() => window.open('tel:+254706020966', '_blank')}>
+                            <div className="text-x13 lg:text-sm text-rose-500 inline-flex items-center justify-center">
                                 <RiPhoneLine className="mr-1.5" /> +254 706 020 966
                             </div>
                         </div>
 
-                        <p className="text-x15 text-rose-500 mt-4">Let's work together</p>
+                        <p className="text-x15 lg:text-sm text-rose-500 mt-4">Let's work together</p>
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1800px'
+    },
     fontFamily: {
       sans: [
         'sofia-pro',
@@ -24,6 +32,7 @@ module.exports = {
     },
     extend: {
       fontSize: {
+        'sm': ['0.875rem', '1.5rem'],
         'x13': ['0.8125rem', '1.125rem'],
         'x8': ['0.5rem', '1rem'],
         'x15': ['0.8rem', "1.2rem"]
